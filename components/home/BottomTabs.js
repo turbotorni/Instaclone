03e03aa2d3b15b2420965b77ee7525b2d12b19f1
Dein Name <deine@email.de>
2025-07-30 23:53:20 +0200

    Fix bottom tab bar full width and styling

--- components/home/BottomTabs.js
+++ components/home/BottomTabs.js
@@ -4,29 +4,24 @@ import { Divider } from 'react-native-elements'
 
 export const bottomTabIcons = [
     {
-        name: 'Home',
-        active: 'https://img.icons8.com/?size=100&id=2797&format=png&color=000000',
-        inactive: 'https://img.icons8.com/?size=100&id=83326&format=png&color=000000',
+        name: 'Home', active: 'https://img.icons8.com/?size=48&id=2797&format=png&color=000000',
+        inactive: 'https://img.icons8.com/?size=48&id=83326&format=png&color=000000'
     },
     {
-        name: 'Search',
-        active: 'https://img.icons8.com/?size=100&id=7695&format=png&color=000000',
-        inactive: 'https://img.icons8.com/?size=100&id=59878&format=png&color=000000',
+        name: 'Search', active: 'https://img.icons8.com/?size=48&id=7695&format=png&color=000000',
+        inactive: 'https://img.icons8.com/?size=48&id=59878&format=png&color=000000'
     },
     {
-        name: 'Reels',
-        active: 'https://img.icons8.com/?size=100&id=YoIaSvIehcuI&format=png&color=000000',
-        inactive: 'https://img.icons8.com/?size=100&id=PxI9IPCyBAOD&format=png&color=000000',
+        name: 'Reels', active: 'https://img.icons8.com/?size=48&id=YoIaSvIehcuI&format=png&color=000000',
+        inactive: 'https://img.icons8.com/?size=48&id=PxI9IPCyBAOD&format=png&color=000000'
     },
     {
-        name: 'Shop',
-        active: 'https://img.icons8.com/?size=100&id=8287&format=png&color=000000',
-        inactive: 'https://img.icons8.com/?size=100&id=489&format=png&color=000000',
+        name: 'Shop', active: 'https://img.icons8.com/?size=48&id=8287&format=png&color=000000',
+        inactive: 'https://img.icons8.com/?size=48&id=489&format=png&color=000000'
     },
     {
-        name: 'Profile',
-        active: 'https://img.icons8.com/ios-filled/100/user-male-circle.png',
-        inactive: 'https://img.icons8.com/ios/100/user-male-circle.png',
+        name: 'Profile', active: 'https://img.icons8.com/ios-filled/48/user-male-circle.png',
+        inactive: 'https://img.icons8.com/ios/48/user-male-circle.png'
     },
 ]
 
@@ -34,12 +29,17 @@ const BottomTabs = () => {
     const [activeTab, setActiveTab] = useState('Home')
 
     const Icon = ({ icon }) => (
-        <TouchableOpacity onPress={() => setActiveTab(icon.name)}>
+        <TouchableOpacity onPress={() => setActiveTab(icon.name)} style={styles.iconContainer}>
             <Image
                 source={{ uri: activeTab === icon.name ? icon.active : icon.inactive }}
                 style={[
                     styles.icon,
-                    icon.name === 'Profile' ? styles.profilePic(activeTab === 'Profile') : null,
+                    icon.name === 'Profile' && {
+                        borderRadius: 50,
+                        borderWidth: activeTab === 'Profile' ? 2 : 0,
+                        borderColor: '#fff',
+                        backgroundColor: '#000',
+                    },
                 ]}
             />
         </TouchableOpacity>
@@ -47,13 +47,14 @@ const BottomTabs = () => {
 
     return (
         <View style={styles.wrapper}>
-            <Divider width={1} orientation="vertical" />
+            <Divider width={1} orientation="horizontal" color="#333" style={{ width: '100%' }} />
             <View style={styles.container}>
                 {bottomTabIcons.map((icon, index) => (
                     <Icon key={index} icon={icon} />
                 ))}
             </View>
         </View>
+
     )
 }
 
@@ -68,18 +69,19 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        height: 50,
+        alignItems: 'center',
+        height: 60,
         paddingTop: 10,
+        width: '100%',
+    },
+    iconContainer: {
+        padding: 8,
     },
     icon: {
         width: 30,
         height: 30,
     },
-    profilePic: (isActive) => ({
-        borderRadius: 50,
-        borderWidth: isActive ? 2 : 0,
-        borderColor: '#fff',
-    }),
 })
 
 export default BottomTabs
+
